423578f70e838a945d28cecfa72328178eab08d5
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2024-12-02 20:51:22 +0000

    day 1 solution

---
+++ day-01/main.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple
+
+
+def part_1(input: str) -> None:
+    left, right = split_lists(input)
+    return sum([abs(x - y) for x, y in zip(sorted(left), sorted(right))])
+
+
+def part_2(input: str) -> None:
+    left, right = split_lists(input)
+    right_count = {}
+    for i in right:
+        if not right_count.get(i):
+            right_count[i] = 0
+        right_count[i] += 1
+    return sum([x * right_count.get(x, 0) for x in left])
+
+
+def split_lists(input: str) -> Tuple[List[int], List[int]]:
+    return sorted(input[0::2]), sorted(input[1::2])
+
+
+if __name__ == "__main__":
+    input_file = "./input.txt"
+    input = [*map(int, open(input_file).read().split())]
+
+    print(part_1(input))
+    print(part_2(input))
