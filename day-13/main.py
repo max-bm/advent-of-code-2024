38ada195c41e61973d763ef759fdff7cd24b2611
Max Buckmire-Monro <maxbuckmiremonro@gmail.com>
2024-12-13 08:17:03 +0000

    day 13 solution tidy

--- day-13/main.py
+++ day-13/main.py
@@ -5,7 +5,7 @@ def part_1(input: str) -> int:
     machines = input.split("\n\n")
     cost = 0
     for m in machines:
-        ax, ay, bx, by, px, py = [*map(int, re.findall(r"((?:\+|\-)?\d+)", m))]
+        ax, ay, bx, by, px, py = map(int, re.findall(r"((?:\+|\-)?\d+)", m))
         na = (bx * py - by * px) / (bx * ay - by * ax)
         nb = (ax * py - ay * px) / (ax * by - ay * bx)
         if not na.is_integer() or not nb.is_integer():
@@ -18,7 +18,7 @@ def part_2(input: str) -> int:
     machines = input.split("\n\n")
     cost = 0
     for m in machines:
-        ax, ay, bx, by, px, py = [*map(int, re.findall(r"((?:\+|\-)?\d+)", m))]
+        ax, ay, bx, by, px, py = map(int, re.findall(r"((?:\+|\-)?\d+)", m))
         px += 10000000000000
         py += 10000000000000
         na = (bx * py - by * px) / (bx * ay - by * ax)
